da13fe2f0e94b9cefaf5641f412bba9e14eaba87
Nikhil <nikhilpillay1@gmail.com>
2025-11-17 22:12:42 +0200

    Updated dockerfile to include frontend build

--- api/aggregator-api/src/test/java/com/nikhilpillay/aggregator/AggregatorApplicationTests.java
+++ api/aggregator-api/src/test/java/com/nikhilpillay/aggregator/AggregatorApplicationTests.java
@@ -1,13 +1,8 @@
 package com.nikhilpillay.aggregator;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AggregatorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
